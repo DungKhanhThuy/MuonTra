e9d1222f444ac8098f99a8be6401eb1b3aa765ea
anhdung1329 <nguyenanhdung132@gmail.com>
2020-12-25 17:20:32 +0700

    start

---
+++ GiangDuong/GiangDuong/ConnectDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace GiangDuong
+{
+    public class ConnectDB
+    {
+        SqlConnection cn = new SqlConnection();
+        static public String getconnect()
+        {
+            return (@"Data Source=.\SQLEXPRESS; Initial Catalog=QLTBGD; Integrated Security=true");
+        }
+    }
+}
---
+++ GiangDuong/GiangDuong/DangNhap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace GiangDuong
+{
+    public class DangNhap
+    {
+        public bool DangNhapHT(string Username, string Pass)
+        {
+            string sql = "SELECT * FROM NguoiDung WHERE MaNV='" + Username + "' AND MatKhau='" + Pass + "'";
+            SqlConnection con = new SqlConnection(ConnectDB.getconnect());
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(sql, con);
+
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
