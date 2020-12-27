b751363b77c6ea38e886563cea25ae48cc918e5f
anhdung1329 <nguyenanhdung132@gmail.com>
2020-12-27 21:27:19 +0700

    test

---
+++ GiangDuong/GiangDuong/ClassPhong.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GiangDuong
+{
+    class ClassPhong
+    {
+        ConnectDB cn = new ConnectDB();
+        public DataTable Show()
+        {
+            return cn.LoadData("HienThi_Phong");
+        }
+
+        public void ThemLoai(string maphong)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                SqlConnection cn = new SqlConnection(ConnectDB.getconnect());
+                cn.Open();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "ThemPhong";
+                command.Connection = cn;
+                command.Parameters.AddWithValue("@MaPhong", SqlDbType.VarChar).Value = maphong;
+
+
+                command.ExecuteNonQuery();
+                cn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void SuaLoai(string maphong)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                SqlConnection cn = new SqlConnection(ConnectDB.getconnect());
+                cn.Open();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "SuaPhong";
+                command.Connection = cn;
+                command.Parameters.AddWithValue("@MaPhong", SqlDbType.VarChar).Value = maphong;
+                command.ExecuteNonQuery();
+                cn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
