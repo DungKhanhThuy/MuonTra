ac8c39610c5969f4cfa0b9482b6dbeebe51f5ebc
anhdung1329 <nguyenanhdung132@gmail.com>
2020-12-28 00:25:46 +0700

    thoat - phan quyen - nguoi dung

--- GiangDuong/GiangDuong/frmDangNhap.cs
+++ GiangDuong/GiangDuong/frmDangNhap.cs
@@ -18,13 +18,24 @@ namespace GiangDuong
         }
         DangNhap dn = new DangNhap();
 
+        public class bientoancuc
+        {
+            public static bool ad = false;
+        }
+
         private void btnDangNhap_Click(object sender, EventArgs e)
         {
             if (dn.DangNhapHT(txtUserName.Text, txtPass.Text) == true)
             {
                 MessageBox.Show("Bạn đăng nhập thành công ^^", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                frmMuonTra MuonTra = new frmMuonTra();
-                MuonTra.Show();
+                ConnectDB cn = new ConnectDB();
+                DataTable dt = cn.LoadData1("HienThi_MatKhau_LaAdmin", "@MaNV", txtUserName.Text);
+                if (dt.Rows[0][2].ToString() == "co")
+                {
+                    bientoancuc.ad = true;
+                }
+                frmHocVien hv = new frmHocVien();
+                hv.Show();
                 Hide();
             }
             else MessageBox.Show("Tên đăng nhập hoặc mật khẩu sai. Mời bạn nhập lại !!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
--- GiangDuong/GiangDuong/frmNguoiDung.cs
+++ GiangDuong/GiangDuong/frmNguoiDung.cs
@@ -74,7 +74,7 @@ namespace GiangDuong
 
         private void loạiToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            frmNguoiDung hd = new frmNguoiDung();
+            frmLoai hd = new frmLoai();
             hd.Show();
             Hide();
         }
@@ -95,9 +95,179 @@ namespace GiangDuong
 
         private void ngườiDùngToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            frmLoai hd = new frmLoai();
+            frmNguoiDung hd = new frmNguoiDung();
             hd.Show();
             Hide();
         }
+
+        //==================================
+
+        ClassNguoiDung nguoidung = new ClassNguoiDung();
+        int chon;
+        ConnectDB cn = new ConnectDB();
+
+
+        public void KhoiTao()
+        {
+            comboBoxMaNV.Enabled = textMatKhau.Enabled = comboBoxLaAdmin.Enabled  = false;
+            buttonThem.Enabled = buttonSua.Enabled = buttonXoa.Enabled = true;
+            buttonLuu.Enabled = buttonHuy.Enabled = false;
+        }
+
+        //Mo cac button enable
+        public void Mo()
+        {
+            comboBoxMaNV.Enabled = textMatKhau.Enabled = comboBoxLaAdmin.Enabled = true;
+            buttonThem.Enabled = buttonSua.Enabled = buttonXoa.Enabled = false;
+            buttonLuu.Enabled = buttonHuy.Enabled = true;
+        }
+
+        public void SetNull()
+        {
+            comboBoxMaNV.Text = textMatKhau.Text = comboBoxLaAdmin.Text = "";
+        }
+
+
+
+        private void dataGridViewNguoiDung_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                comboBoxMaNV.Text = dataGridViewNguoiDung.Rows[e.RowIndex].Cells[0].Value.ToString();
+                textMatKhau.Text = dataGridViewNguoiDung.Rows[e.RowIndex].Cells[1].Value.ToString();
+                comboBoxLaAdmin.Text = dataGridViewNguoiDung.Rows[e.RowIndex].Cells[2].Value.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void buttonThem_Click(object sender, EventArgs e)
+        {
+            Mo();
+            SetNull();
+            chon = 1;
+            DataTable dt = cn.LoadData("HienThi_NguoiDung");
+            comboBoxMaNV.Text = dt.Rows[0][0].ToString();
+        }
+
+        private void buttonHuy_Click(object sender, EventArgs e)
+        {
+            frmNguoiDung_Load(sender, e);
+            SetNull();
+        }
+
+        private void buttonSua_Click(object sender, EventArgs e)
+        {
+            Mo();
+            comboBoxMaNV.Enabled = false;
+            chon = 2;
+        }
+
+        private void buttonXoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cn.LoadData1("XemNguoiDung", "@MaNV", comboBoxMaNV.Text).Rows.Count == 0)
+                    MessageBox.Show("Không tìm thấy Nhân viên này");
+                else
+                {
+                    if (DialogResult.Yes == MessageBox.Show("Bạn có muốn xóa không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+                    {
+                        cn.Xoa("XoaNguoiDung", "@MaNV", comboBoxMaNV.Text);
+                        MessageBox.Show("Xóa thành công!");
+                        frmNguoiDung_Load(sender, e);
+                        SetNull();
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void frmNguoiDung_Load(object sender, EventArgs e)
+        {
+            KhoiTao();
+            dataGridViewNguoiDung.DataSource = nguoidung.Show();
+            chon = 0;
+            HienThi_MaNV();
+        }
+
+        private void buttonLuu_Click(object sender, EventArgs e)
+        {
+            if (chon == 1) //Them
+            {
+                if (cn.LoadData1("XemNguoiDung", "@MaNV", comboBoxMaNV.Text).Rows.Count > 0)
+                    MessageBox.Show("Tài khoản đã có trong danh sách");
+                else
+                {
+                    if (textMatKhau.Text == "" || comboBoxLaAdmin.Text == "" )
+                        MessageBox.Show("Vui lòng nhập đầy đủ thông tin");
+                    else
+                    {
+                        nguoidung.NguoiDung_DB("ThemNguoiDung", comboBoxMaNV.Text, textMatKhau.Text, comboBoxLaAdmin.Text);
+                        MessageBox.Show("Thêm thành công");
+                        frmNguoiDung_Load(sender, e);
+                    }
+                }
+
+            }
+            else if (chon == 2) //Sua
+            {
+                if (cn.LoadData1("XemNguoiDung", "@MaNV", comboBoxMaNV.Text).Rows.Count == 0)
+                    MessageBox.Show("Tài khoản này chưa có trong danh sách");
+                else
+                {
+                    if (textMatKhau.Text == "" || comboBoxLaAdmin.Text == "" )
+                        MessageBox.Show("Vui lòng nhập đầy đủ thông tin");
+                    else
+                    {
+                        if (DialogResult.Yes == MessageBox.Show("Bạn có muốn sửa học viên này?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+                        {
+                            nguoidung.NguoiDung_DB("SuaNguoiDung", comboBoxMaNV.Text, textMatKhau.Text, comboBoxLaAdmin.Text);
+                            MessageBox.Show("Sửa thành công");
+                            frmNguoiDung_Load(sender, e);
+                        }
+                    }
+                }
+
+            }
+        }
+
+        public void HienThi_MaNV()
+        {
+            comboBoxMaNV.DataSource = cn.LoadData("HienThi_NhanVien");
+            comboBoxMaNV.DisplayMember = "MaNV";
+            comboBoxMaNV.ValueMember = "MaNV";
+            comboBoxMaNV.SelectedValue = "MaNV";
+            comboBoxMaNV.SelectedIndex = 0;
+
+        }
+
+
+        private void comboBoxMaNV_SelectedValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dt = cn.LoadData1("HienThi_MatKhau_LaAdmin", "@MaNV", comboBoxMaNV.Text);
+                if (dt.Rows.Count != 0)
+                {
+                    if (comboBoxMaNV.SelectedItem != null)
+                    {
+                        textMatKhau.Text = dt.Rows[0][1].ToString();
+                        comboBoxLaAdmin.Text = dt.Rows[0][2].ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
     }
 }
