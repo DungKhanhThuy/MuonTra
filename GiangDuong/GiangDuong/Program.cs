ec1433da561e5c88394b0a670874a5c4e23830c1
anhdung1329 <nguyenanhdung132@gmail.com>
2020-12-29 08:13:14 +0700

    pro

--- GiangDuong/GiangDuong/Program.cs
+++ GiangDuong/GiangDuong/Program.cs
@@ -16,7 +16,7 @@ namespace GiangDuong
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmSuCo());
+            Application.Run(new frmDangNhap());
         }
     }
 }
